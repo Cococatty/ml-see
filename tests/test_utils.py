270c644e061b366d78b4ef00d5c6c041c88c0cdb
Cococatty <carinauc@outlook.com>
2023-08-12 23:44:56 +1200

    set up unit test env

---
+++ tests/test_utils.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+import sys
+from pathlib import Path
+FILE = Path(__file__).resolve()
+test_dir = FILE.parents[0]
+project_dir = FILE.parents[1]
+sys.path.append(str(project_dir))
+sys.path.append(str(test_dir))
+os.chdir(test_dir)
+import logging
+logger = logging.getLogger('unittest_log')
+# project modules
+from utils import load_json
+
+
+class TestUtilities(unittest.TestCase):
+    def test_givenAJSONFile_whenJSONDataIsRequired_thenLoadJSONData(self):
+        expected_result = './outputs'
+        json_data = load_json(os.path.join(project_dir, 'configs.json'))
+        self.assertEqual(json_data['common_att']['output_path'], expected_result)
